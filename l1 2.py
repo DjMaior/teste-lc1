31320265087283a0cb5edd5d07090a13af7b0892
DjMaior <72826693+DjMaior@users.noreply.github.com>
2020-10-15 18:33:17 -0300

    Add files via upload

---
+++ l1 2.py
@@ -0,0 +1,8 @@
+#você precisa comentar duas linhas deste programa
+#para que o mesmo possa gerar a saíída esperada pela avaliação automática
+
+
+A,B = input("").split()
+area = int(A)*int(B)
+
+print(area)
